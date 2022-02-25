a12380363b6a27c73bada33410652025eca9e5db
nenormalnui <72319525+nenormalnui@users.noreply.github.com>
2022-02-25 20:42:09 +0300

    Update getGoods.js

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,77 +1,61 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
-    const viewAll = document.querySelectorAll('.more');
 
-    renderGoods = (goods) => {
+    const renderGoods = (goods) => {
         const goodsContainer = document.querySelector('.long-goods-list');
 
-        goodsContainer.innerHTML = '';
+        goodsContainer.innerHTML = "";
 
         goods.forEach(good => {
             const goodBlock = document.createElement('div');
 
             goodBlock.classList.add('col-lg-3');
-            goodBlock.classList.add('col-sm-3');
+            goodBlock.classList.add('col-sm-6');
 
             goodBlock.innerHTML = `
-                     <div class="goods-card">
-						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
-                        <img src="db/${good.img}" alt="${good.label}" class="goods-image">
-						<h3 class="goods-title">${good.name}</h3>
-						<p class="goods-description">${good.description}</p>
-						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
-							<span class="button-price">$${good.price}</span>
-						</button>
-					</div>
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
             `;
 
             goodsContainer.append(goodBlock);
-        })
-    }
-
-    const getData = (value, category) => {
-        fetch('https://test-9b0a7-default-rtdb.europe-west1.firebasedatabase.app/db.json') 
+        });
+    };
+    
+    const getData = (value, category) => {        
+        fetch('db/db.json')
         .then((res) => res.json())
         .then((data) => {
             const array = category ? data.filter((item) => item[category] === value) : data;
-
+            
             localStorage.setItem('goods', JSON.stringify(array));
 
-            if (window.location.pathname !== "/goods.html") {
-                window.location.href = 'goods.html';
+            if (window.location.pathname !== "/wildberriesjs/goods.html") {
+                window.location.href = '/wildberriesjs/goods.html';
             } else {
                 renderGoods(array);
-            }
-        })
-    }
+            }            
+        });
+    };
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
-            event.preventDefault(); 
-
+            event.preventDefault();
             const linkValue = link.textContent;
             const category = link.dataset.field;
-
             getData(linkValue, category);
-        })
-    })
+        });
+    });
 
-    viewAll.forEach((link) => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault(); 
-
-            const linkValue = link.textContent;
-            const category = link.dataset.field;
-
-            getData(linkValue, category);
-        })
-    })
-
-    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+    if (localStorage.getItem('goods') && window.location.pathname === "/wildberriesjs/goods.html") {
         renderGoods(JSON.parse(localStorage.getItem('goods')));
     }
-
-
-}
+};
 
 getGoods();
