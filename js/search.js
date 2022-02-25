abd3045cdb69afab8fa06671581d1ed0862d0000
nenormalnui <72319525+nenormalnui@users.noreply.github.com>
2022-02-25 20:38:22 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -30,7 +30,7 @@ const search = function() {
     };
     
     const getData = (value) => {        
-        fetch('/db/db.json')
+        fetch('https://test-9b0a7-default-rtdb.europe-west1.firebasedatabase.app/db.json')
         .then((res) => res.json())
         .then((data) => {
 
