ffc389d0353a0ae9398257880477e545be7ddaa1
nenormalnui <72319525+nenormalnui@users.noreply.github.com>
2022-02-25 20:25:31 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,9 +1,7 @@
 const search = function() {
     const input = document.querySelector('.search-block > input')
     const searchBtn = document.querySelector('.search-block > button')
-/* input.addEventListener('input', (event) => {
-        console.log(event.target.value);
-    })*/
+
     const renderGoods = (goods) => {
         const goodsContainer = document.querySelector('.long-goods-list');
 
